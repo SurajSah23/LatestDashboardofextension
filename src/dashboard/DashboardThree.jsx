5a9ed0dc2f9635ed429f6ce2c3003752df9004bd
Suraj <suraj23@navgurukul.org>
2025-02-03 15:37:01 +0530

    updated

--- src/dashboard/DashboardThree.jsx
+++ src/dashboard/DashboardThree.jsx
@@ -28,21 +28,21 @@ function DashboardThree() {
         </div>
 
         {/* Credit */}
-        <div className="flex items-center bg-blue-50 p-3 rounded-xl">
+        <div className="flex items-center bg-pink-50 p-3 rounded-xl">
           <img
             src="https://i.ibb.co/QbD7jVY/Featured-icon-2.png"
             alt="Credit Card"
-            className="h-15 ml-2 p-2 sm:h-16"
+            className="h-10 sm:h-12"
           />
           <div className="flex-1">
             <div className="flex items-center justify-between mb-2 relative bottom-2">
-              <span className="p-4 -ml-2 text-blue-500">Verification</span>
+              <span className="p-4 -ml-2 text-blue-500">Credit</span>
               <span className="text-sm text-blue-500 relative right-10 -mr-5">
                 2/100
               </span>
             </div>
-            <div className="w-full sm:w-[213px] bg-gray-100 rounded-full h-1.5 relative right-3 bottom-6 ml-2">
-              <div className="bg-blue-500 h-1.5 rounded-full w-[75%]"></div>
+            <div className="w-full sm:w-[213px] bg-gray-100 rounded-full h-1.5 relative right-3 bottom-6 ml-5">
+              <div className="bg-pink-500 h-1.5 rounded-full w-[75%]"></div>
             </div>
           </div>
         </div>
@@ -52,7 +52,7 @@ function DashboardThree() {
           <img
             src="https://i.ibb.co/Cd0sg2b/Featured-icon-5.png"
             alt="Verification"
-            className="h-15 ml-2 p-2 sm:h-16"
+            className="h-10 sm:h-12"
           />
           <div className="flex-1">
             <div className="flex items-center justify-between mb-2 relative bottom-2">
@@ -61,7 +61,7 @@ function DashboardThree() {
                 2/100
               </span>
             </div>
-            <div className="w-full sm:w-[213px] bg-gray-100 rounded-full h-1.5 relative right-3 bottom-6 ml-2">
+            <div className="w-full sm:w-[213px] bg-gray-100 rounded-full h-1.5 relative right-3 bottom-6 ml-5">
               <div className="bg-blue-500 h-1.5 rounded-full w-[75%]"></div>
             </div>
           </div>
